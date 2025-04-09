dce56ad9f388f79547fab83404b52d93247d8d02
BadalSK <140627997+BadalSK@users.noreply.github.com>
2025-04-09 21:38:57 +0530

    Add files via upload

---
+++ Day 20 Aggregate Functions in SQL.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products(
+   product_id SERIAL PRIMARY KEY,
+   product_name VARCHAR(100),
+   category VARCHAR(50),
+   price NUMERIC(10,2),
+   quantity INT,
+   added_date DATE,
+   discount_rate NUMERIC(5,2)
+); 
+
+INSERT INTO products(product_name,category,price,quantity,added_date,discount_rate)
+VALUES('Laptop', 'Electronics', 75000.50, 10, '2024-01-15', 10.00),
+('Smartphone', 'Electronics', 45000.99, 25, '2024-02-20', 5.00),
+('Headphones', 'Accessories', 1500.75, 50, '2024-03-05', 15.00),
+('Office Chair', 'Furniture', 5500.00, 20, '2023-12-01', 20.00),
+('Desk', 'Furniture', 8000.00, 15, '2023-11-20', 12.00),
+('Monitor', 'Electronics', 12000.00, 8, '2024-01-10', 8.00),
+('Printer', 'Electronics', 9500.50, 5, '2024-02-01', 7.50),
+('Mouse', 'Accessories', 750.00, 40, '2024-03-18', 10.00),
+('Keyboard', 'Accessories', 1250.00, 35, '2024-03-18', 10.00),
+('Tablet', 'Electronics', 30000.00, 12, '2024-02-28', 5.00);
+
+SELECT * FROM products;
+
+-- AGGREGATE FUNCTION IN SQL: --
+
+--Total Quantity Available of all products
+SELECT SUM(quantity) AS total_quantity
+FROM products;
+
+SELECT SUM(quantity) AS total_ele_quantity
+FROM products
+WHERE category ='Electronics' AND price > 20000;
+
+-- Total Number of products
+
+SELECT COUNT(*) AS total_products
+FROM products;
+
+-- Count with Condition
+SELECT COUNT(*) AS total_products
+FROM products
+WHERE product_name LIKE '%phone';
+
+SELECT COUNT(*) AS total_category
+FROM products
+WHERE category LIKE '%A%';
+
+SELECT * FROM products;
+
+-- Average Price of Products
+SELECT AVG(price) AS average_price
+FROM products;
+
+-- Average Price of Products with condition
+SELECT AVG(price) AS average_price
+FROM products
+WHERE category = 'Accessories' OR added_date > '2024-02-01';
+
+-- Maximum and Minimum price
+SELECT MAX(price) AS max_price,
+       MIN(price) AS min_price
+FROM products;	   
+       
+SELECT * FROM products;
+
+SELECT MAX(discount_rate) AS max_discount,
+       MIN(discount_rate) AS min_discount
+FROM products
+WHERE category = 'Electronics';
+       
